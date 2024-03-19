45ea7ee4c9ebce9227650ba521bda734a083949a
paulalfille <paulalfille@penguin>
2024-03-19 13:01:08 -0400

    Updated code to ES6 style (class, const and let, no var, and some arrow notation)

--- pinhole.js
+++ pinhole.js
@@ -4,43 +4,71 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-Pinhole = (function(){
-    function Pinhole(){
+class Pinhole {
+    constructor(){
         this.lines = [];
         this.stack = [];
     }
-    Pinhole.prototype.begin = function(){
+    
+    begin(){
         this.stack.push(this.lines.length);
     }
-    Pinhole.prototype.end = function(){
+    
+    end(){
         this.stack.pop();
     }
-    Pinhole.prototype.rotate = function(x, y, z){
-        var i = 0;
-        if (this.stack.length > 0) {
-            i = this.stack[this.stack.length-1];
-        }
-        for (; i < this.lines.length; i++) {
+    
+    _stackstart(){
+		if (this.stack.length==0) {
+			return 0 ;
+		}
+		return this.stack[this.stack.length-1];
+	}
+    
+    rotate(x, y, z){
+        for (let i = this._stackstart(); i < this.lines.length; i++) {
             if (x != 0) {
-                this.lines[i].p1 = rotate(this.lines[i].p1, x, 0);
-                this.lines[i].p2 = rotate(this.lines[i].p2, x, 0);
+                this.lines[i].p1 = this._rotateX(this.lines[i].p1, x);
+                this.lines[i].p2 = this._rotateX(this.lines[i].p2, x);
             }
             if (y != 0) {
-                this.lines[i].p1 = rotate(this.lines[i].p1, y, 1);
-                this.lines[i].p2 = rotate(this.lines[i].p2, y, 1);
+                this.lines[i].p1 = this._rotateY(this.lines[i].p1, y);
+                this.lines[i].p2 = this._rotateY(this.lines[i].p2, y);
             }
             if (z != 0) {
-                this.lines[i].p1 = rotate(this.lines[i].p1, z, 2);
-                this.lines[i].p2 = rotate(this.lines[i].p2, z, 2);
+                this.lines[i].p1 = this._rotateZ(this.lines[i].p1, z);
+                this.lines[i].p2 = this._rotateZ(this.lines[i].p2, z);
             }
         }
     }
-    Pinhole.prototype.translate = function(x, y, z){
-        var i = 0;
-        if (this.stack.length > 0) {
-            i = this.stack[this.stack.length-1];
-        }
-        for (; i < this.lines.length; i++) {
+
+    // https://www.siggraph.org/education/materials/HyperGraph/modeling/mod_tran/3drota.htm
+    _rotateX(p, q){
+        return {
+			x:p.x,
+			y:p.y*Math.cos(q) - p.z*Math.sin(q),
+			z:p.y*Math.sin(q) + p.z*Math.cos(q)
+			}
+    }
+
+    _rotateY(p, q){
+        return {
+			x:p.z*Math.sin(q) + p.x*Math.cos(q),
+			y:p.y,
+			z:p.z*Math.cos(q) - p.x*Math.sin(q)
+			}
+    }
+
+    _rotateZ(p, q){
+        return {
+			x:p.x*Math.cos(q) - p.y*Math.sin(q),
+			y:p.x*Math.sin(q) + p.y*Math.cos(q),
+			z:p.z
+			}
+    }
+    
+    translate(x, y, z){
+        for (let i = this._stackstart(); i < this.lines.length; i++) {
             this.lines[i].p1.x += x;
             this.lines[i].p1.y += y;
             this.lines[i].p1.z += z;
@@ -49,12 +77,9 @@ Pinhole = (function(){
             this.lines[i].p2.z += z;
         }
     }
-    Pinhole.prototype.scale = function(x, y, z){
-        var i = 0;
-        if (this.stack.length > 0) {
-            i = this.stack[this.stack.length-1];
-        }
-        for (; i < this.lines.length; i++) {
+    
+    scale(x, y, z){
+        for (let i = this._stackstart(); i < this.lines.length; i++) {
             this.lines[i].p1.x *= x;
             this.lines[i].p1.y *= y;
             this.lines[i].p1.z *= z;
@@ -63,27 +88,25 @@ Pinhole = (function(){
             this.lines[i].p2.z *= z;
         }
     }
-    Pinhole.prototype.colorize = function(color){
-        var i = 0;
-        if (this.stack.length > 0) {
-            i = this.stack[this.stack.length-1];
-        }
-        for (; i < this.lines.length; i++) {
+    
+    colorize(color){
+        for (let i = this._stackstart(); i < this.lines.length; i++) {
             this.lines[i].color = color;
         }
     }
-    Pinhole.prototype.center = function(){
-        var i = 0;
-        if (this.stack.length > 0) {
-            i = this.stack[this.stack.length-1];
-        }
-        var minx = Number.POSITIVE_INFINITY;
-        var miny = Number.POSITIVE_INFINITY;
-        var minz = Number.POSITIVE_INFINITY;
-        var maxx = Number.NEGATIVE_INFINITY;
-        var maxy = Number.NEGATIVE_INFINITY;
-        var maxz = Number.NEGATIVE_INFINITY;
-        for (; i < this.lines.length; i++) {
+    
+    center(){
+		let i = this._stackstart() ;
+		if ( i >= this.lines.length ) {
+			return ;
+		}
+        let minx = Number.POSITIVE_INFINITY;
+        let miny = Number.POSITIVE_INFINITY;
+        let minz = Number.POSITIVE_INFINITY;
+        let maxx = Number.NEGATIVE_INFINITY;
+        let maxy = Number.NEGATIVE_INFINITY;
+        let maxz = Number.NEGATIVE_INFINITY;
+        for ( ; i < this.lines.length; i++) {
             if (this.lines[i].p1.x < minx) {
                 minx = this.lines[i].p1.x;
             }
@@ -121,12 +144,13 @@ Pinhole = (function(){
                 maxz = this.lines[i].p2.z;
             }
         }
-        var x = (maxx + minx) / 2;
-        var y = (maxy + miny) / 2;
-        var z = (maxz + minz) / 2;
+        const x = (maxx + minx) / 2;
+        const y = (maxy + miny) / 2;
+        const z = (maxz + minz) / 2;
         this.translate(-x, -y, -z);
     }
-    Pinhole.prototype.drawCube = function(minx, miny, minz, maxx, maxy, maxz) {
+    
+    drawCube(minx, miny, minz, maxx, maxy, maxz) {
         this.drawLine(minx, maxy, minz, maxx, maxy, minz)
         this.drawLine(maxx, maxy, minz, maxx, miny, minz)
         this.drawLine(maxx, miny, minz, minx, miny, minz)
@@ -140,11 +164,13 @@ Pinhole = (function(){
         this.drawLine(maxx, miny, minz, maxx, miny, maxz)
         this.drawLine(minx, miny, minz, minx, miny, maxz)
     }
-    Pinhole.prototype.drawDot = function(x, y, z, radius){
+    
+    drawDot(x, y, z, radius){
         this.drawLine(x, y, z, x, y, z)
         this.lines[this.lines.length-1].scale = 10 / 0.1 * radius;
     }
-    Pinhole.prototype.drawLine = function(x1, y1, z1, x2, y2, z2) {
+    
+    drawLine(x1, y1, z1, x2, y2, z2) {
         this.lines.push({
             p1:{x:x1,y:y1,z:z1},
             p2:{x:x2,y:y2,z:z2},
@@ -152,13 +178,14 @@ Pinhole = (function(){
             scale:1
         })
     }
-    Pinhole.prototype.drawCircle = function(x, y, z, radius) {
-        var f, l;
-        var first, prev;
-        var circleSteps = this.circleSteps||45;
+    
+    drawCircle(x, y, z, radius) {
+        let f, l;
+        let first, prev;
+        const circleSteps = this.circleSteps||45;
         // we go one beyond the steps because we need to join at the end
-        for (var i = 0; i <= circleSteps; i++) {
-            var d = destination(x, y, (Math.PI*2)/circleSteps*i, radius);
+        for (let i = 0; i <= circleSteps; i++) {
+            const d = this._destination(x, y, (Math.PI*2)/circleSteps*i, radius);
             d.z = z
             if (i > 0) {
                 if (i == circleSteps) {
@@ -166,7 +193,7 @@ Pinhole = (function(){
                 } else {
                     this.drawLine(l.x, l.y, l.z, d.x, d.y, d.z);
                 }
-                var line = this.lines[this.lines.length-1]
+                const line = this.lines[this.lines.length-1]
                 line.nocaps = false;
                 //line.circle = true;
                 if (!first) {
@@ -185,13 +212,14 @@ Pinhole = (function(){
             l = d;
         }
     }
-    Pinhole.prototype.drawEllipse = function(x, y, z, radiusa, radiusb) {
-        var f, l;
-        var first, prev;
-        var circleSteps = this.circleSteps||45;
+    
+    drawEllipse(x, y, z, radiusa, radiusb) {
+        let f, l;
+        let first, prev;
+        const circleSteps = this.circleSteps||45;
         // we go one beyond the steps because we need to join at the end
-        for (var i = 0; i <= circleSteps; i++) {
-            var d = destination2(x, y, (Math.PI*2)/circleSteps*i, radiusa, radiusb);
+        for (let i = 0; i <= circleSteps; i++) {
+            const d = this._destination2(x, y, (Math.PI*2)/circleSteps*i, radiusa, radiusb);
             d.z = z
             if (i > 0) {
                 if (i == circleSteps) {
@@ -199,7 +227,7 @@ Pinhole = (function(){
                 } else {
                     this.drawLine(l.x, l.y, l.z, d.x, d.y, d.z);
                 }
-                var line = this.lines[this.lines.length-1]
+                const line = this.lines[this.lines.length-1]
                 line.nocaps = false;
                 //line.circle = true;
                 if (!first) {
@@ -218,31 +246,33 @@ Pinhole = (function(){
             l = d;
         }
     }
-    function capsInsert(caps, x, y, z){
-        var key = x+":"+y+":"+z;
+    
+    _capsInsert(caps, x, y, z){
+        const key = x+":"+y+":"+z;
         if (!caps[key]){
             caps[key] = true;
             return true;
         }
         return false;
     }
-    function compareLines(a, b){
-        var az = Math.min(a.p1.z, a.p2.z);
-        var bz = Math.min(b.p1.z, b.p2.z);
+    
+    _compareLines(a, b){
+        const az = Math.min(a.p1.z, a.p2.z);
+        const bz = Math.min(b.p1.z, b.p2.z);
         if (az > bz){
             return -1;
         } else if (az < bz) {
             return +1;
         }
-        var ay = Math.min(a.p1.y, a.p2.y);
-        var by = Math.min(b.p1.z, b.p2.y);
+        const ay = Math.min(a.p1.y, a.p2.y);
+        const by = Math.min(b.p1.z, b.p2.y);
         if (ay < by){
             return -1;
         } else if (ay > by) {
             return +1;
         }
-        var ax = Math.min(a.p1.x, a.p2.x);
-        var bx = Math.min(b.p1.x, b.p2.x);
+        const ax = Math.min(a.p1.x, a.p2.x);
+        const bx = Math.min(b.p1.x, b.p2.x);
         if (ax < bx){
             return -1;
         } else if (ax > bx) {
@@ -250,44 +280,45 @@ Pinhole = (function(){
         }
         return 0;
     }
-    Pinhole.prototype.render = function(canvas,opts){
-        var optsScale = (opts?opts.scale:null)||1;
-        var optsLineWidth = (opts?opts.lineWidth:null)||1;
-        var optsBGColor = (opts?opts.bgColor:null)||'white';
-        var width = canvas.width;
-        var height = canvas.height;
-        var c = canvas.getContext("2d")
-        this.lines.sort(compareLines);
-        for (var i=0;i<this.lines.length;i++){
+    
+    render(canvas,opts){
+        const optsScale = (opts?opts.scale:null)||1;
+        const optsLineWidth = (opts?opts.lineWidth:null)||1;
+        const optsBGColor = (opts?opts.bgColor:null)||'white';
+        const width = canvas.width;
+        const height = canvas.height;
+        const c = canvas.getContext("2d")
+        this.lines.sort(this._compareLines);
+        for (let i=0;i<this.lines.length;i++){
             this.lines[i].drawcoords = null;
         }
         if (optsBGColor) {
             c.fillStyle = optsBGColor;
             c.fillRect(0, 0, width, height);
         }
-        var capsMap = {};
-        var caps;
-        var ccolor;
-        var focal = Math.min(width, height) / 2;
-        function maybeDraw(line) {
-            var p1 = projectPoint(line.p1.x, line.p1.y, line.p1.z, width, height, focal, optsScale)
-            var p2 = projectPoint(line.p2.x, line.p2.y, line.p2.z, width, height, focal, optsScale)
-            var t1 = lineWidthAtZ(line.p1.z, focal) * optsLineWidth * line.scale;
-            var t2 = lineWidthAtZ(line.p2.z, focal) * optsLineWidth * line.scale;
-            var cap1, cap2;
+        const capsMap = {};
+        let caps;
+        let ccolor;
+        const focal = Math.min(width, height) / 2;
+        const maybeDraw = (line) => {
+            const p1 = this._projectPoint(line.p1.x, line.p1.y, line.p1.z, width, height, focal, optsScale)
+            const p2 = this._projectPoint(line.p2.x, line.p2.y, line.p2.z, width, height, focal, optsScale)
+            const t1 = this._lineWidthAtZ(line.p1.z, focal) * optsLineWidth * line.scale;
+            const t2 = this._lineWidthAtZ(line.p2.z, focal) * optsLineWidth * line.scale;
+            let cap1, cap2;
             if (!line.nocaps) {
-                cap1 = capsInsert(caps, p1.x, p1.y, p1.z);
-                cap2 = capsInsert(caps, p2.x, p2.y, p2.z);
+                cap1 = this._capsInsert(caps, p1.x, p1.y, p1.z);
+                cap2 = this._capsInsert(caps, p2.x, p2.y, p2.z);
             }
-            return drawUnbalancedLineSegment(c,
+            return this._drawUnbalancedLineSegment(c,
                 p1.x, p1.y, p2.x, p2.y,
                 t1, t2,
                 cap1, cap2,
                 line.circle
             )
         }
-        for (var i=0;i<this.lines.length;i++){
-            var line = this.lines[i];
+        for (let i=0;i<this.lines.length;i++){
+            const line = this.lines[i];
             if (line.color != ccolor) {
                 ccolor = line.color;
                 caps = capsMap[ccolor];
@@ -302,8 +333,8 @@ Pinhole = (function(){
                     // need to process the coords for all segments belonging to
                     // the current circle segment.
                     // first get the basic estimates
-                    var coords = [];
-                    var seg = line.cfirst;
+                    const coords = [];
+                    const seg = line.cfirst;
                     while (seg){
                         seg.drawcoords = maybeDraw(seg);
                         if (!seg.drawcoords) {
@@ -314,18 +345,18 @@ Pinhole = (function(){
 
                     }
                     // next reprocess to join the midpoints
-                    for (var j = 0; j < coords.length;j++){
-                        var line1, line2;
+                    for (let j = 0; j < coords.length;j++){
+                        let line1, line2;
                         if (j == 0) {
                             line1 = coords[coords.length-1];
                         } else {
                             line1 = coords[j-1];
                         }
                         line2 = coords[j];
-                        var midx1 = (line2.x1 + line1.x4) / 2;
-                        var midy1 = (line2.y1 + line1.y4) / 2;
-                        var midx2 = (line2.x2 + line1.x3) / 2;
-                        var midy2 = (line2.y2 + line1.y3) / 2;
+                        const midx1 = (line2.x1 + line1.x4) / 2;
+                        const midy1 = (line2.y1 + line1.y4) / 2;
+                        const midx2 = (line2.x2 + line1.x3) / 2;
+                        const midy2 = (line2.y2 + line1.y3) / 2;
                         line2.x1 = midx1;
                         line2.y1 = midy1;
                         line1.x4 = midx1;
@@ -350,7 +381,8 @@ Pinhole = (function(){
             }
         }
     }
-    function drawUnbalancedLineSegment(c, x1, y1, x2, y2, t1, t2, cap1, cap2, circleSegment) {
+    
+    _drawUnbalancedLineSegment(c, x1, y1, x2, y2, t1, t2, cap1, cap2, circleSegment) {
         if (x1 == x2 && y1 == y2) {
             c.beginPath();
             c.arc(x1, y1, t1/2, 0, 2 * Math.PI, false);
@@ -358,15 +390,15 @@ Pinhole = (function(){
             c.fill();
             return null;
         }
-        var a = lineAngle(x1, y1, x2, y2);
-        var d1 = destination(x1, y1, a-Math.PI/2, t1/2);
-        var d2 = destination(x1, y1, a+Math.PI/2, t1/2);
-        var d3 = destination(x2, y2, a+Math.PI/2, t2/2);
-        var d4 = destination(x2, y2, a-Math.PI/2, t2/2);
+        const a = this._lineAngle(x1, y1, x2, y2);
+        const d1 = this._destination(x1, y1, a-Math.PI/2, t1/2);
+        const d2 = this._destination(x1, y1, a+Math.PI/2, t1/2);
+        const d3 = this._destination(x2, y2, a+Math.PI/2, t2/2);
+        const d4 = this._destination(x2, y2, a-Math.PI/2, t2/2);
         if (circleSegment) {
             return {x1:d1.x, y1:d1.y, x2:d2.x, y2:d2.y, x3:d3.x, y3:d3.y, x4:d4.x, y4:d4.y};
         }
-        var cubicCorner = 1.0 / 3 * 2; //0.552284749831
+        const cubicCorner = 1.0 / 3 * 2; //0.552284749831
         if (cap1 && t1 < 2) {
             cap1 = false;
         }
@@ -376,16 +408,16 @@ Pinhole = (function(){
         c.beginPath();
         c.moveTo(d1.x, d1.y);
         if (cap1) {
-            var a1 = destination(d1.x, d1.y, a-Math.PI*2, t1*cubicCorner);
-            var a2 = destination(d2.x, d2.y, a-Math.PI*2, t1*cubicCorner);
+            const a1 = this._destination(d1.x, d1.y, a-Math.PI*2, t1*cubicCorner);
+            const a2 = this._destination(d2.x, d2.y, a-Math.PI*2, t1*cubicCorner);
             c.bezierCurveTo(a1.x, a1.y, a2.x, a2.y, d2.x, d2.y);
         } else {
             c.lineTo(d2.x, d2.y);
         }
         c.lineTo(d3.x, d3.y);
         if (cap2) {
-            var a1 = destination(d3.x, d3.y, a-Math.PI*2, -t2*cubicCorner);
-            var a2 = destination(d4.x, d4.y, a-Math.PI*2, -t2*cubicCorner);
+            const a1 = this._destination(d3.x, d3.y, a-Math.PI*2, -t2*cubicCorner);
+            const a2 = this._destination(d4.x, d4.y, a-Math.PI*2, -t2*cubicCorner);
             c.bezierCurveTo(a1.x, a1.y, a2.x, a2.y, d4.x, d4.y);
         } else {
             c.lineTo(d4.x, d4.y);
@@ -400,7 +432,7 @@ Pinhole = (function(){
     //     X is left/right
     //     Y is down/up
     //     Z is near/far, the 0 position is focal distance away from lens.
-    function projectPoint(
+    _projectPoint(
         x, y, z, // 3d point to project
         w, h, f, // width, height, focal
         scale // scale
@@ -408,58 +440,36 @@ Pinhole = (function(){
         x = x*scale*f;
         y = y*scale*f;
         z = z*scale*f;
-        var zz = z + f;
+        let zz = z + f;
         if (zz == 0) {
             zz = Math.MIN_VALUE;
         }
-        var p = {};
+        const p = {};
         p.x = x*(f/zz) + w/2;
         p.y = y*(f/zz) - h/2;
         p.y *= -1;
         return p;
     }
     
-    function lineWidthAtZ(z, f) {
+    _lineWidthAtZ(z, f) {
         return ((z*-1 + 1) / 2) * f * 0.04;
     }
     
-    function lineAngle(x1, y1, x2, y2){
+    _lineAngle(x1, y1, x2, y2){
         return Math.atan2(y1-y2, x1-x2)
     }
     
-    function destination(x, y, angle, distance){
+    _destination(x, y, angle, distance){
         return {
             x: x + Math.cos(angle)*distance,
             y: y + Math.sin(angle)*distance,
         }
     }
-    function destination2(x, y, angle, distancea, distanceb){
+    
+    _destination2(x, y, angle, distancea, distanceb){
         return {
             x: x + Math.cos(angle)*distancea,
             y: y + Math.sin(angle)*distanceb,
         }
     }
-    // https://www.siggraph.org/education/materials/HyperGraph/modeling/mod_tran/3drota.htm
-    function rotate(p, q, which) {
-        var dx, dy, dz;
-        switch (which) {
-        case (0): // x
-            dy = p.y*Math.cos(q) - p.z*Math.sin(q);
-            dz = p.y*Math.sin(q) + p.z*Math.cos(q);
-            dx = p.x;
-            break;
-        case (1): // y
-            dz = p.z*Math.cos(q) - p.x*Math.sin(q);
-            dx = p.z*Math.sin(q) + p.x*Math.cos(q);
-            dy = p.y;
-            break;
-        case (2): // z
-            dx = p.x*Math.cos(q) - p.y*Math.sin(q);
-            dy = p.x*Math.sin(q) + p.y*Math.cos(q);
-            dz = p.z;
-            break;
-        }
-        return {x:dx,y:dy,z:dz}
-    }
-    return Pinhole
-}())
+}
